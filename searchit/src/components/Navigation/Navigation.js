12c06e339deef8d444ec8504afb61352a1295a75
milktegi <milkteegi@gmail.com>
2019-01-29 09:14:15 +0900

    added basic layout

---
+++ searchit/src/components/Navigation/Navigation.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Navigation = () => {
+	return(
+		<nav style={{display: 'flex', justifyContent: 'flex-end'}}>
+			<p className='f5 link dim black underline pa3 pointer'>로그아웃</p>
+		</nav>
+	)
+}
+
+
+export default Navigation;
